2ea7b50365ab91211dec52a9acade96c72a1f8d4
rhyusarmiento <rhyu.sar.15@gmail.com>
2023-08-12 07:47:51 -0600

    moving to render

--- src/API_URL.js
+++ src/API_URL.js
@@ -1,1 +1,3 @@
-export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api"
+// export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000/api"
+
+export const API_URL = process.env.REACT_APP_API_URL || "https://rs-capstone-backend.onrender.com/api"
